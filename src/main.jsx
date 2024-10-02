fa466330ec4dc54f52291e12ff00d6c3d919c49d
nandini <nandinibag92@gmail.com>
2024-10-02 15:34:02 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
-import { AppProvider } from './Context';
 
 createRoot(document.getElementById('root')).render(  
   <StrictMode>
-  <AppProvider>
   <GlobalStyle/>
     <App/>
-  </AppProvider>
   </StrictMode>,
 )
